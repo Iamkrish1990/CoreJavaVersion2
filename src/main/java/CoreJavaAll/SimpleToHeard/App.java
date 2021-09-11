67bdfa61780e94421ca5af28fc95b5ce9467e0e3
Krishnendu <krishnendunandi1990@gmail.com>
2021-09-11 22:05:10 +0530

    First project str

---
+++ src/main/java/CoreJavaAll/SimpleToHeard/App.java
@@ -0,0 +1,13 @@
+package CoreJavaAll.SimpleToHeard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
